d0dffab374fc77dbb659e913b22a5fa822a5ff15
Sami Ahvenniemi <spahvenn@gmail.com>
2023-01-04 18:54:51 +0200

    Fix header responsiveness styles

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -3,37 +3,39 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import { Grid } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 
 export default function NavigationBar() {
   return (
     <AppBar position="sticky">
-      <Grid container>
-        <Grid item xs={12} ml={3} mr={3}>
-          <Box
-            sx={{ display: "flex", alignItems: "center" }}
-            minHeight={(theme) => theme.spacing(8)}
-          >
-            <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
-              <Typography variant="h6" component="div" mr={2}>
-                Home
-              </Typography>
-              <Typography variant="h6" component="div">
-                Gallery
-              </Typography>
-            </Box>
+      <Container maxWidth="lg">
+        <Grid container>
+          <Grid item xs={12}>
             <Box
-              sx={{
-                display: "flex",
-                justifyContent: "flex-end",
-                width: "100%",
-              }}
+              sx={{ display: "flex", alignItems: "center" }}
+              minHeight={(theme) => theme.spacing(8)}
             >
-              <ShoppingCartIcon />
+              <Box sx={{ display: "flex", justifyContent: "flex-start" }}>
+                <Typography variant="h6" component="div" mr={2.5}>
+                  Home
+                </Typography>
+                <Typography variant="h6" component="div">
+                  Gallery
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  width: "100%",
+                }}
+              >
+                <ShoppingCartIcon />
+              </Box>
             </Box>
-          </Box>
+          </Grid>
         </Grid>
-      </Grid>
+      </Container>
     </AppBar>
   );
 }
