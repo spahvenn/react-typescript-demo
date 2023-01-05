1282943e5c44bf54277a2d8ede5d032e66776b4b
Sami Ahvenniemi <spahvenn@gmail.com>
2023-01-05 10:37:46 +0200

    Refactored carousel only to contain the image, not the text

--- src/components/FrontPageCarousel.tsx
+++ src/components/FrontPageCarousel.tsx
@@ -7,6 +7,7 @@ import {
   Typography,
 } from "@mui/material";
 import Carousel from "react-material-ui-carousel";
+import { pagePreviews } from "../utils/utils";
 
 const CallToActionButton = styled(Button)(({ theme }) => ({
   padding: "0.5rem 1.25rem",
@@ -41,101 +42,112 @@ function CarouselImage({ carouselItem }: { carouselItem: any }) {
 
 const carouselItems = [
   {
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEX9jsp60DxonQFt6hBcd4szSEdgv6tUdgw8n5NObQtqtLEo8NpIVL-soCGKTz7kApaxpcohtZHGUR0Y9X57FxK1ec83fbOz6dVnsCb5o3ipD_F6T7-dL8yD0SmV0x4F0YcrydahEqUOCaqfNEUwF4mNcg=w3730-h2100-no?authuser=0",
+    src: pagePreviews.winter.src,
   },
   {
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEXRi7cjNArOloX0YgE8iQn0_d-AuuFVPkNuYplBzuN_7dQhHTVIDSvZ540dU5VfGbMMViXaxkKlbVCkNE0Dt9fiDGKmqXzdrjnrOtU8sGxJFWT9edNY0y2fm0M1PflzzOfy2y2xEycv-SWsaQMW75IHaw=w3730-h2100-no?authuser=0",
+    src: pagePreviews.spring.src,
+  },
+  {
+    src: pagePreviews.summer.src,
+  },
+  {
+    src: pagePreviews.autumn.src,
   },
 ];
 
+function DesktopCarouselContainer({ carouselItems }: { carouselItems: any[] }) {
+  return (
+    <Box display={{ xs: "none", sm: "block" }}>
+      <Grid container>
+        <Grid item xs={7} sm={8} md={9}>
+          <Carousel indicators={false}>
+            {carouselItems.map((carouselItem) => {
+              return (
+                <Box height="400px">
+                  <CarouselImage carouselItem={carouselItem}></CarouselImage>
+                </Box>
+              );
+            })}
+          </Carousel>
+        </Grid>
+        <Grid
+          item
+          xs={5}
+          sm={4}
+          md={3}
+          bgcolor={(theme) => theme.palette.primary.main}
+        >
+          <Box
+            height="100%"
+            sx={{
+              padding: 3,
+              boxSizing: "border-box",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <Box sx={{ textAlign: "middle" }}>
+              <Typography
+                fontSize="1.2rem"
+                mb={2}
+                color={(theme) => theme.palette.primary.contrastText}
+                textAlign="center"
+              >
+                The best seasonal image collections!
+              </Typography>
+              <Box textAlign="center">
+                <CallToActionButton variant="outlined">
+                  View now
+                </CallToActionButton>
+              </Box>
+            </Box>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+function MobileCarouselContainer({ carouselItems }: { carouselItems: any }) {
+  return (
+    <Box display={{ xs: "block", sm: "none" }}>
+      <Carousel indicators={false}>
+        {carouselItems.map((carouselItem: any) => {
+          return (
+            <Box height="220px">
+              <CarouselImage carouselItem={carouselItem} />
+            </Box>
+          );
+        })}
+      </Carousel>
+      <Box
+        sx={(theme) => ({
+          backgroundColor: theme.palette.primary.main,
+        })}
+      >
+        <Box padding={1.5}>
+          <Typography
+            fontSize="1.2rem"
+            mb={1.5}
+            color={(theme) => theme.palette.primary.contrastText}
+            textAlign="center"
+          >
+            The best seasonal image collections!
+          </Typography>
+          <Box textAlign="center">
+            <CallToActionButton variant="outlined">View now</CallToActionButton>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
 export default function FrontPageCarousel() {
   return (
-    <Carousel autoPlay={false}>
-      {carouselItems.map((carouselItem) => {
-        return (
-          <>
-            <Box display={{ xs: "none", sm: "block" }}>
-              <Grid container>
-                <Grid item xs={7} sm={8} md={9}>
-                  <Box height="400px">
-                    <CardMedia
-                      style={{
-                        display: "block",
-                        height: "100%",
-                        backgroundSize: "cover",
-                        backgroundRepeat: "no-repeat",
-                        backgroundPosition: "center center",
-                      }}
-                      component="div"
-                      image={carouselItem.src}
-                    />
-                  </Box>
-                </Grid>
-                <Grid
-                  item
-                  xs={5}
-                  sm={4}
-                  md={3}
-                  bgcolor={(theme) => theme.palette.primary.main}
-                >
-                  <Box
-                    height="100%"
-                    sx={{
-                      padding: 3,
-                      boxSizing: "border-box",
-                      display: "flex",
-                      alignItems: "center",
-                    }}
-                  >
-                    <CarouselImage carouselItem={carouselItem}></CarouselImage>
-                    <Box sx={{ textAlign: "middle" }}>
-                      <Typography
-                        fontSize="1.2rem"
-                        mb={2}
-                        color={(theme) => theme.palette.primary.contrastText}
-                        textAlign="center"
-                      >
-                        The best seasonal image collections!
-                      </Typography>
-                      <Box textAlign="center">
-                        <CallToActionButton variant="outlined">
-                          View now
-                        </CallToActionButton>
-                      </Box>
-                    </Box>
-                  </Box>
-                </Grid>
-              </Grid>
-            </Box>
-            <Box display={{ xs: "block", sm: "none" }}>
-              <Box height="220px">
-                <CarouselImage carouselItem={carouselItem} />
-              </Box>
-              <Box
-                sx={(theme) => ({
-                  backgroundColor: theme.palette.primary.main,
-                })}
-              >
-                <Box padding={1.5}>
-                  <Typography
-                    fontSize="1.2rem"
-                    mb={1.5}
-                    color={(theme) => theme.palette.primary.contrastText}
-                    textAlign="center"
-                  >
-                    The best seasonal image collections!
-                  </Typography>
-                  <Box textAlign="center">
-                    <CallToActionButton variant="outlined">
-                      View now
-                    </CallToActionButton>
-                  </Box>
-                </Box>
-              </Box>
-            </Box>
-          </>
-        );
-      })}
-    </Carousel>
+    <>
+      <MobileCarouselContainer carouselItems={carouselItems} />
+      <DesktopCarouselContainer carouselItems={carouselItems} />
+    </>
   );
 }
---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+export const pagePreviews = {
+  spring: {
+    title: "Spring",
+    description:
+      "Browser our catalog of Spring images! Amazing landscapes blossoming life!",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEX9jsp60DxonQFt6hBcd4szSEdgv6tUdgw8n5NObQtqtLEo8NpIVL-soCGKTz7kApaxpcohtZHGUR0Y9X57FxK1ec83fbOz6dVnsCb5o3ipD_F6T7-dL8yD0SmV0x4F0YcrydahEqUOCaqfNEUwF4mNcg=w3730-h2100-no?authuser=0",
+  },
+  summer: {
+    title: "Summer",
+    description:
+      "Browser our catalog of Summer images! Pick you favourite sunny landscapes!",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEXDKIL0j35rRGj_oDNDnL17tYmm-dtSeNlKp1rHFQUzDd9Qz3CxdwpsWPRks-4jy3KS5mS-dmnbcxoYwv63BSc41z57irTkr_irGzp-VfxNxkRV2N2lvphTd3Le0GYqe5VZOnM4CV7l2cixqZawFOXqfg=w3730-h2100-no?authuser=0",
+  },
+  autumn: {
+    title: "Autumn",
+    description:
+      "Browser our catalog of Autumn images! Pick you vivid Autumn landscapes!",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEXFOYb3rq2r5yoJiiC4rJ-9fOzRj7g_PV5anWMbYWQiFmQuaXADvntQGNR3bUNIuv4Bhp_6MRKDkPxwo92DhY-c48PW-9kSE4_SZd858-NrFI6ui7Vi8KpInq2B1EiYM2uBKA8hKn9TRPPZps01qJQsqg=w3730-h2100-no?authuser=0",
+  },
+  winter: {
+    title: "Winter",
+    description:
+      "Browser our catalog of Winter images! Frosty landscapes for all uses!",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEWnEw6nznkffDQJbQPu9nEUa1fEUk-jAvZHgQkFwTwzrO6oiVDPCWm9HnTOl1h0kIO0_1sZRp4q_5hA18wb7UEAUqgvk2NA-u2AddDvPDYGDTNl-ulMXvBmIXNqm6jde3DKcsAtDJuVSYnR-vdjN-rt4A=w3730-h2100-no?authuser=0",
+  },
+};
