404c7ad0537dd9b8b97ada31a41892b162f06995
Sami Ahvenniemi <spahvenn@gmail.com>
2023-01-11 12:58:59 +0200

    Saving generalStore to localStorage and hydrate on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { ThemeProvider } from "@emotion/react";
 import { createTheme, CssBaseline } from "@mui/material";
 import router from "./utils/router";
 import { RouterProvider } from "react-router-dom";
-import { RootStoreProvider, useGeneralStore } from "./mobx/RootStore";
-import { useMemo } from "react";
+import {
+  RootStoreProvider,
+  useGeneralStore,
+  useRootStore,
+} from "./mobx/RootStore";
+import { useEffect, useMemo } from "react";
 import { observer } from "mobx-react-lite";
+import { reaction, set } from "mobx";
 
 export function App() {
   return (
@@ -15,6 +20,7 @@ export function App() {
 }
 
 const AppWithStoreAccess = observer(() => {
+  const rootStore = useRootStore();
   const generalStore = useGeneralStore();
   const theme = useMemo(
     () =>
@@ -25,6 +31,27 @@ const AppWithStoreAccess = observer(() => {
       }),
     [generalStore.uiMode]
   );
+
+  useEffect(() => {
+    reaction(
+      () => JSON.stringify(rootStore.generalStore),
+      (json) => {
+        localStorage.setItem("generalStore", json);
+      }
+    );
+    const jsonString = localStorage.getItem("generalStore");
+    if (!jsonString) {
+      return;
+    }
+    const json = JSON.parse(jsonString);
+    let obj = Object.getOwnPropertyNames(generalStore);
+    obj.forEach((property) => {
+      if (json?.hasOwnProperty(property)) {
+        set(generalStore, property, json[property]);
+      }
+    });
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
