fe4965b9312170d085d6ea848ecd04e718632828
Sami Ahvenniemi <spahvenn@gmail.com>
2023-01-19 11:27:18 +0200

    Fix carousel large image resolutions

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,7 +6,7 @@ export const pagePreviews = {
     title: "Spring",
     description:
       "Browser our catalog of Spring images! Amazing landscapes blossoming life!",
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEX9jsp60DxonQFt6hBcd4szSEdgv6tUdgw8n5NObQtqtLEo8NpIVL-soCGKTz7kApaxpcohtZHGUR0Y9X57FxK1ec83fbOz6dVnsCb5o3ipD_F6T7-dL8yD0SmV0x4F0YcrydahEqUOCaqfNEUwF4mNcg=w3730-h2100-no?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEX9jsp60DxonQFt6hBcd4szSEdgv6tUdgw8n5NObQtqtLEo8NpIVL-soCGKTz7kApaxpcohtZHGUR0Y9X57FxK1ec83fbOz6dVnsCb5o3ipD_F6T7-dL8yD0SmV0x4F0YcrydahEqUOCaqfNEUwF4mNcg=w1800-no?authuser=0",
     srcSmall:
       "https://lh3.googleusercontent.com/pw/AL9nZEX9jsp60DxonQFt6hBcd4szSEdgv6tUdgw8n5NObQtqtLEo8NpIVL-soCGKTz7kApaxpcohtZHGUR0Y9X57FxK1ec83fbOz6dVnsCb5o3ipD_F6T7-dL8yD0SmV0x4F0YcrydahEqUOCaqfNEUwF4mNcg=w1000-no?authuser=0",
     btnUrl: "/gallery?season=spring",
@@ -15,7 +15,7 @@ export const pagePreviews = {
     title: "Summer",
     description:
       "Browser our catalog of Summer images! Pick you favourite sunny landscapes!",
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEXDKIL0j35rRGj_oDNDnL17tYmm-dtSeNlKp1rHFQUzDd9Qz3CxdwpsWPRks-4jy3KS5mS-dmnbcxoYwv63BSc41z57irTkr_irGzp-VfxNxkRV2N2lvphTd3Le0GYqe5VZOnM4CV7l2cixqZawFOXqfg=w3730-h2100-no?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEXDKIL0j35rRGj_oDNDnL17tYmm-dtSeNlKp1rHFQUzDd9Qz3CxdwpsWPRks-4jy3KS5mS-dmnbcxoYwv63BSc41z57irTkr_irGzp-VfxNxkRV2N2lvphTd3Le0GYqe5VZOnM4CV7l2cixqZawFOXqfg=w1800-no?authuser=0",
     srcSmall:
       "https://lh3.googleusercontent.com/pw/AL9nZEXDKIL0j35rRGj_oDNDnL17tYmm-dtSeNlKp1rHFQUzDd9Qz3CxdwpsWPRks-4jy3KS5mS-dmnbcxoYwv63BSc41z57irTkr_irGzp-VfxNxkRV2N2lvphTd3Le0GYqe5VZOnM4CV7l2cixqZawFOXqfg=w1000-no?authuser=0",
     btnUrl: "/gallery?season=summer",
@@ -24,7 +24,7 @@ export const pagePreviews = {
     title: "Autumn",
     description:
       "Browser our catalog of Autumn images! Pick you vivid Autumn landscapes!",
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEXFOYb3rq2r5yoJiiC4rJ-9fOzRj7g_PV5anWMbYWQiFmQuaXADvntQGNR3bUNIuv4Bhp_6MRKDkPxwo92DhY-c48PW-9kSE4_SZd858-NrFI6ui7Vi8KpInq2B1EiYM2uBKA8hKn9TRPPZps01qJQsqg=w3730-h2100-no?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEXFOYb3rq2r5yoJiiC4rJ-9fOzRj7g_PV5anWMbYWQiFmQuaXADvntQGNR3bUNIuv4Bhp_6MRKDkPxwo92DhY-c48PW-9kSE4_SZd858-NrFI6ui7Vi8KpInq2B1EiYM2uBKA8hKn9TRPPZps01qJQsqg=w1800-no?authuser=0",
     srcSmall:
       "https://lh3.googleusercontent.com/pw/AL9nZEXFOYb3rq2r5yoJiiC4rJ-9fOzRj7g_PV5anWMbYWQiFmQuaXADvntQGNR3bUNIuv4Bhp_6MRKDkPxwo92DhY-c48PW-9kSE4_SZd858-NrFI6ui7Vi8KpInq2B1EiYM2uBKA8hKn9TRPPZps01qJQsqg=w1000-no?authuser=0",
     btnUrl: "/gallery?season=autumn",
@@ -33,7 +33,7 @@ export const pagePreviews = {
     title: "Winter",
     description:
       "Browser our catalog of Winter images! Frosty landscapes for all uses!",
-    src: "https://lh3.googleusercontent.com/pw/AL9nZEWnEw6nznkffDQJbQPu9nEUa1fEUk-jAvZHgQkFwTwzrO6oiVDPCWm9HnTOl1h0kIO0_1sZRp4q_5hA18wb7UEAUqgvk2NA-u2AddDvPDYGDTNl-ulMXvBmIXNqm6jde3DKcsAtDJuVSYnR-vdjN-rt4A=w3730-h2100-no?authuser=0",
+    src: "https://lh3.googleusercontent.com/pw/AL9nZEWnEw6nznkffDQJbQPu9nEUa1fEUk-jAvZHgQkFwTwzrO6oiVDPCWm9HnTOl1h0kIO0_1sZRp4q_5hA18wb7UEAUqgvk2NA-u2AddDvPDYGDTNl-ulMXvBmIXNqm6jde3DKcsAtDJuVSYnR-vdjN-rt4A=w1800-no?authuser=0",
     srcSmall:
       "https://lh3.googleusercontent.com/pw/AL9nZEWnEw6nznkffDQJbQPu9nEUa1fEUk-jAvZHgQkFwTwzrO6oiVDPCWm9HnTOl1h0kIO0_1sZRp4q_5hA18wb7UEAUqgvk2NA-u2AddDvPDYGDTNl-ulMXvBmIXNqm6jde3DKcsAtDJuVSYnR-vdjN-rt4A=w1000-no?authuser=0",
     btnUrl: "/gallery?season=winter",
