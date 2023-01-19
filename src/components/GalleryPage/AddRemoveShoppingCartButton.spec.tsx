0f2300ffbe3fd87e1b3d52a7a9b4f04e4a385c97
Sami Ahvenniemi <spahvenn@gmail.com>
2023-01-19 13:44:44 +0200

    Added more depth to tests

--- src/components/GalleryPage/AddRemoveShoppingCartButton.spec.tsx
+++ src/components/GalleryPage/AddRemoveShoppingCartButton.spec.tsx
@@ -1,5 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { Router } from "@remix-run/router";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router";
+import { ToastContainer } from "react-toastify";
 import * as store from "../../mobx/RootStore";
 import { routes } from "../../utils/router";
 
@@ -7,36 +9,60 @@ window.scrollTo = jest.fn();
 
 const shoppingCartItem = { id: 1, src: "", srcSmall: "" };
 
-const router = createMemoryRouter(routes, {
-  initialEntries: ["/gallery?season=spring&pictureId=1"],
-  initialIndex: 1,
-});
+function createRouter() {
+  return createMemoryRouter(routes, {
+    initialEntries: ["/gallery?season=spring&pictureId=1"],
+    initialIndex: 1,
+  });
+}
 
 describe("AddRemoveShoppingCartButton", () => {
-  it("should render add button", () => {
+  let router: Router;
+  beforeEach(() => {
+    router = createRouter();
+  });
+
+  it("should render add button and show toast on click", async () => {
     const testStore = {
       generalStore: {},
-      shoppingCartStore: { shoppingCartItems: [] },
+      shoppingCartStore: { shoppingCartItems: [], add: jest.fn() },
     };
     render(
       <store.RootStoreProvider
         testStore={testStore as unknown as store.RootStore}
       >
+        <ToastContainer />
         <RouterProvider router={router} />
       </store.RootStoreProvider>
     );
-    expect(screen.getByText("Add to cart")).toBeTruthy();
+    fireEvent.click(screen.getByText("Add to cart"));
+    await waitFor(() =>
+      expect(
+        screen.getByText("Picture added to the shopping cart")
+      ).toBeInTheDocument()
+    );
   });
-  it("should render remove button", () => {
+  it("should render remove button and show toast on click", async () => {
     const testStore = {
       generalStore: {},
-      shoppingCartStore: { shoppingCartItems: [shoppingCartItem] },
+      shoppingCartStore: {
+        shoppingCartItems: [shoppingCartItem],
+        remove: jest.fn(),
+      },
     };
     render(
-      <store.RootStoreProvider testStore={testStore as store.RootStore}>
+      <store.RootStoreProvider
+        testStore={testStore as unknown as store.RootStore}
+      >
+        <ToastContainer />
         <RouterProvider router={router} />
       </store.RootStoreProvider>
     );
-    expect(screen.getByText("Remove from cart")).toBeTruthy();
+    fireEvent.click(screen.getByText("Remove from cart"));
+    await waitFor(() =>
+      expect(
+        screen.getByText("Picture removed from the shopping cart")
+      ).toBeInTheDocument()
+    );
   });
 });
